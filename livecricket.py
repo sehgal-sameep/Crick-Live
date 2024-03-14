bc2ba5ca92f36609a0a087cf61edc902a1505057
sehgal-sameep <98826161+sehgal-sameep@users.noreply.github.com>
2024-03-14 23:14:52 +0530

    Updated Code

--- livecricket.py
+++ livecricket.py
@@ -4,12 +4,12 @@ from datetime import datetime
 class ScoreGet:
     def __init__(self):
         self.url_get_all_matches="https://api.cricapi.com/v1/currentMatches"
-        self.apikey="7b040e2e-8704-4350-a5c9-490e6cf4a0df"
+        self.apikey="YOUR API KEY"
 
     def get_details(self):
         uri_params={"apikey":self.apikey}
         # resp=requests.get(self.url_get_all_matches,params=uri_params)
-        resp=requests.get("https://api.cricapi.com/v1/currentMatches?apikey=7b040e2e-8704-4350-a5c9-490e6cf4a0df&offset=0")
+        resp=requests.get("https://api.cricapi.com/v1/currentMatches?apikey=YOUR_API_KEY&offset=0")
         resp_dict=resp.json()
 
 
@@ -19,7 +19,7 @@ class ScoreGet:
             if(i['matchStarted']):
                 todays_date=datetime.today().strftime('%Y-%m-%d')
                 if(todays_date==i['date']):
-                    if(i['venue']=="SCF Cricket Ground, Salem"):
+                    if(i['venue']=="STADIUM_NAME"):
                         data_found=1
                         if(data_found):
                             try:
@@ -41,10 +41,10 @@ if __name__=="__main__":
     whatsapp_message=obj_score.get_details()
 
     from twilio.rest import Client
-    a_sid="ACe4240d82a107d94ffcfc98fc0f55e604"
-    auth_token="c4a69c8d1776582e35d9aa04c4437661"
+    a_sid="Twilio_A_SID"
+    auth_token="Twilio_Auth_Token"
     client=Client(a_sid,auth_token)
-    message= client.messages.create(to='whatsapp:+919034826587', from_='whatsapp:+14155238886',body=whatsapp_message)
+    message= client.messages.create(to='whatsapp:+91CONTACT_No', from_='whatsapp:+14155238886',body=whatsapp_message)
     print(message.sid)
 
 print("\n")
